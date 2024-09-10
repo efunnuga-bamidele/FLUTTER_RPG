04b71c0fa1325cbb7a50a0a3b214b1fd8244b8cf
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-09-10 19:19:55 +0100

    feat:Modify Test

---
+++ lib/constants/custom_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color.fromRGBO(255, 255, 255, 1.0);
+  static Color secondaryColor = const Color.fromRGBO(255, 255, 255, 1.0);
+  static Color backgroundColor = const Color.fromRGBO(0, 0, 0, 1.0);
+  static Color textColor = const Color.fromRGBO(255, 255, 255, 1.0);
+
+  static Color redColor = const Color.fromRGBO(230, 0, 0, 1.0);
+  static Color greenColor = const Color.fromRGBO(0, 230, 0, 1.0);
+  static Color blueColor = const Color.fromRGBO(0, 0, 230, 1.0);
+
+  static Color darkColor = const Color.fromRGBO(0, 0, 0, 1.0);
+  static Color lightColor = const Color.fromRGBO(255, 255, 255, 1.0);
+
+  static Color darkBackgroundColor = const Color.fromRGBO(0, 0, 0, 1.0);
+  static Color lightBackgroundColor = const Color.fromRGBO(255, 255, 255, 1.0);
+
+  static Color darkTextColor = const Color.fromRGBO(255, 255, 255, 1.0);
+  static Color lightTextColor = const Color.fromRGBO(0, 0, 0, 1.0);
+
+  static Color darkSecondaryColor = const Color.fromRGBO(255, 255, 255, 1.0);
+  static Color lightSecondaryColor = const Color.fromRGBO(0, 0, 0, 1.0);
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_rpg/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('SandBox Widget Tests', () {
+    testWidgets('SandBox Widget renders correctly',
+        (WidgetTester tester) async {
+      // Build the widget
+      await tester.pumpWidget(const MaterialApp(home: SandBox()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Find the AppBar
+      expect(find.byType(AppBar), findsOneWidget);
+      expect(find.text('SandBox'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Find the body Text
+      expect(find.text('SandBox'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Check AppBar background color
+      final appBar = tester.widget<AppBar>(find.byType(AppBar));
+      expect(appBar.backgroundColor, Colors.grey);
+    });
   });
 }
